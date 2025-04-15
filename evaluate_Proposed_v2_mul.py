94d0f5ba8b6d813c941d66499ba84021d4baf300
junseo0421 <jmdmk9st@naver.com>
2025-04-15 17:07:03 +0900

    test

--- evaluate_Proposed_v2_mul.py
+++ evaluate_Proposed_v2_mul.py
@@ -8,6 +8,7 @@ import skimage
 from skimage import io
 import skimage.transform
 import time
+import torchvision.transforms.functional as T_FUNC
 
 from utils.utils import *
 
@@ -16,30 +17,67 @@ from models.SRN.SRN import *
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 
+class Margin:
+    def __init__(self, top=0, left=0, bottom=0, right=0):
+        self.top, self.left = top, left
+        self.bottom, self.right = bottom, right
+
+
+def random_square():
+    img_height, img_width = 256, 256
+
+    h = torch.tensor([128])  # 128
+    w = torch.tensor([128])  # 128
+
+    t = 64
+    l = 64
+
+    margin = Margin(t, l, img_height - 128 - t, img_width - 128 - l)
+
+    return (t, l, h, w), margin  # margin : (0, 32, 0, 32)
+
+
 # Evaluate function
 def evaluate(gen, eval_loader, rand_pair, save_dir):
     gen.eval()
 
     com_total = 0
-    for batch_idx, (gt, iner_img, mask_img, name, fol) in enumerate(eval_loader):
+
+    bbox, margin = random_square()  # (t,l,h,w), margin : (64, 64, 64, 64)
+    mask = bbox2mask(bbox, args)  # (1, 1, 128, 128) 부분이 1
+
+    # mask = torch.zeros(size=(3, 256, 256))
+    # mask[:, 64:192, 64:192] = 1
+
+    mask = torch.tensor(1 - mask, requires_grad=False).cuda()  # 192 192
+
+    for batch_idx, (gt, iner_img, resize_img, name, fol) in enumerate(eval_loader):
 
         os.makedirs(join(save_dir, fol[0]), exist_ok=True)
         imgSize = gt.shape[2]
 
-        gt, iner_img, mask_img = Variable(gt).cuda(), Variable(iner_img).cuda(), Variable(
-            mask_img.type(torch.FloatTensor)).cuda()
+        gt, iner_img, resize_img = Variable(gt).cuda(), Variable(iner_img).cuda(), Variable(resize_img).cuda()
+
+        # batch_incomplete = gt[:, :, margin.top:margin.top + 128, margin.left:margin.left + 128]  # 128 x 128
 
         with torch.no_grad():
             t_start = time.time()
 
-            I_pred, _ = gen(mask_img)  # u-net 전용
+            I_pred, _ = gen(iner_img, mask, margin)  # batch_incomplete, mask, margin
+
+            # batch_complete = I_pred * mask + resize_img * (1 - mask)
+            batch_complete_crop = I_pred[:, :, 64:192, 32:224]
+
+            batch_complete_crop = T_FUNC.resize(batch_complete_crop, [192, 192])
+
+            batch_complete_crop[:, :, :, 32:160] = resize_img
 
             t_end = time.time()
             comsum = t_end - t_start
             com_total += comsum
 
         for i in range(gt.size(0)):
-            pre_img = np.transpose(I_pred[i].data.cpu().numpy(), (1, 2, 0))
+            pre_img = np.transpose(batch_complete_crop[i].data.cpu().numpy(), (1, 2, 0))
             std_ = np.expand_dims(np.expand_dims(np.array(std), 0), 0)
             mean_ = np.expand_dims(np.expand_dims(np.array(mean), 0), 0)
             real = np.transpose(gt[i].data.cpu().numpy(), (1, 2, 0))
@@ -76,12 +114,12 @@ if __name__ == '__main__':
 
     p = argparse.ArgumentParser()
     # p.add_argument('--data', type=str, default='/media/yui/Disk/data/cat2dog/')
-    p.add_argument('--epoch', type=int, default=600)
+    p.add_argument('--epoch', type=int, default=400)
     p.add_argument('--pretrained_network', type=int, default=0, help="1: to pretrain network, 0: to finetune network")
     # p.add_argument('--weightpath', type=str, help="specify if pretrained_network=0")
     p.add_argument('--mode', type=str, default='test', choices=['train', 'test'])
-    p.add_argument('--img_shapes', nargs='+', default=[192, 192, 3])
-    p.add_argument('--mask_shapes', nargs='+', default=[192, 128])
+    p.add_argument('--img_shapes', nargs='+', default=[256, 256, 3])
+    p.add_argument('--mask_shapes', nargs='+', default=[128, 128])
     p.add_argument('--batch_size', type=int, default=8)
     p.add_argument('--feat_expansion_op', type=str, default='subpixel')
     p.add_argument('--use_cn', type=int, default=1)
@@ -109,25 +147,21 @@ if __name__ == '__main__':
     # p.add_argument('--summarydir', type=str, default='log/store')
     args = p.parse_args()
 
-
     # Experiment settings
-    pred_step = 1
     times = 1
-    input_size = [128, 86, 56, 38]
     mean = [0.5, 0.5, 0.5]
     std = [0.5, 0.5, 0.5]
 
-
     # List of epochs to test
-    epoch_list = list(range(350, 650, 50))
-    # epoch_list = [550, 600]
+    # epoch_list = list(range(350, 650, 50))
+    epoch_list = [100]
 
     # Load data
     print('Loading data...')
-    transformations = transforms.Compose([Resize((192, 128)), ToTensor(), Normalize(mean, std)])
+    transformations1 = transforms.Compose([Resize((128, 128)), ToTensor(), Normalize(mean, std)])
+    transformations2 = transforms.Compose([Resize((192, 128)), ToTensor(), Normalize(mean, std)])
     tds = glob(args.test_data_dir, '*/*.bmp', True)
-    eval_data = dataset_test4(root=args.test_data_dir, transforms=transformations, imgSize=192,
-                              inputsize=input_size[times - 1], pred_step=pred_step, imglist=tds)
+    eval_data = dataset_test4(root=args.test_data_dir, transforms1=transformations1, transforms2=transformations2, imglist=tds)
     eval_loader = DataLoader(eval_data, batch_size=1, shuffle=False)
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
